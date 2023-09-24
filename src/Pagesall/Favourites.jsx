30ee95dca9abda706c18f6db5950d19f23e1b715
opu3199 <mdhassan.rakib2018@gmail.com>
2023-09-24 17:53:46 +0600

    finished

--- src/Pagesall/Favourites.jsx
+++ src/Pagesall/Favourites.jsx
@@ -30,13 +30,18 @@ const Favourites = () => {
             {nofound ? <p className="text-3xl font-bold flex justify-center items-center">No Data Found </p> :<div className="grid mx-5 md:grid-cols-3 lg:mx-20 gap-10 my-10 ">
                 {
                   isshow ?favourites.map(phone=><Phonecard key={phone.id} phone={phone}></Phonecard>)  : 
-                  favourites.slice(0,2).map(phone=><Phonecard key={phone.id} phone={phone}></Phonecard>) 
+                  favourites.slice(0,3).map(phone=><Phonecard key={phone.id} phone={phone}></Phonecard>) 
                 }
+                
                 </div>}
-                <div className="flex  justify-center my-8">
+                {
+                   favourites.length > 2 &&   <div className="flex  justify-center my-8">
             
-                <button onClick={()=>setisshow(!isshow)} className="bg-emerald-500 p-2 rounded-lg font-bold">Show All</button>
-                </div>
+                   <button onClick={()=>setisshow(!isshow)} className="bg-emerald-500 p-2 rounded-lg font-bold">{isshow ? 'See Less':'See More'}</button>
+                   </div>
+                }
+               
+                
                
         </div>
     );
